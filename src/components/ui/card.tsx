0a4138e14db39a5c48f291b3a951b5c5fe564c5d
Khellermann <khellermann@gmail.com>
2026-06-10 07:33:33 -0300

    Add card component

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+function Card({ className, ...props }: React.ComponentProps<"div">) {
+  return <div className={cn("bg-card text-card-foreground rounded-lg border shadow-sm", className)} {...props} />;
+}
+
+function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
+  return <div className={cn("flex flex-col gap-1.5 p-5", className)} {...props} />;
+}
+
+function CardTitle({ className, ...props }: React.ComponentProps<"h3">) {
+  return <h3 className={cn("text-base font-semibold leading-none tracking-normal", className)} {...props} />;
+}
+
+function CardDescription({ className, ...props }: React.ComponentProps<"p">) {
+  return <p className={cn("text-sm text-muted-foreground", className)} {...props} />;
+}
+
+function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+  return <div className={cn("p-5 pt-0", className)} {...props} />;
+}
+
+function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
+  return <div className={cn("flex items-center p-5 pt-0", className)} {...props} />;
+}
+
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle };
